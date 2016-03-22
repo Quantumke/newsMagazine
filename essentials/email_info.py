c8530d238032d6bcca0a77daa5877fc5a09a2ec1
quantumke <nguruben@gmail.com>
2016-03-22 16:40:01 +0300

    Python project start

---
+++ essentials/email_info.py
@@ -0,0 +1,8 @@
+#--------------------------------------------------------------------------------------------------------------------python send email()
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_PASSWORD = 'bensonngurumburu'
+EMAIL_HOST_USER = 'nguruben@gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
---
+++ publication/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from django_mongoengine import Document, EmbeddedDocument, DynamicDocument
+from django_mongoengine import fields
+from datetime import datetime, timedelta
+from django.utils import timezone
+from django.db.models import permalink
+
+
+# Create your models here
+
+class auth_login(models.Model):
+	user= models.OneToOneField(User)
+	
+	def __unicode__(self):
+		return self.user.username
