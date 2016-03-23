5ff5df3a7274c74a7761e6d0ab13d38773ed16f4
quantumke <nguruben@gmail.com>
2016-03-23 11:56:41 +0300

    save user to postgresql

---
+++ publication/authentication/save_user.py
@@ -0,0 +1,12 @@
+from publication.models import *
+from django.contrib.auth.models import User
+class SaveUser():
+	def run (data):
+		userdata= data.get('user_data')
+		new_user= User(**userdata)
+		password=(data.get('random_password'))
+		password=new_user.password
+		new_user.set_password(password)
+		# new_user.save()
+	
+		# print(password, username)
