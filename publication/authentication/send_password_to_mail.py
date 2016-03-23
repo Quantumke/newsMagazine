3cd6ea62e63fe76b7a4b98ac87d4f4e4b7d1d5ad
quantumke <nguruben@gmail.com>
2016-03-23 17:16:26 +0300

    added username send

--- publication/authentication/send_password_to_mail.py
+++ publication/authentication/send_password_to_mail.py
@@ -9,11 +9,12 @@ class SendPasswordToMail():
 		new_user= User(**user_data)
 		password=(data.get('random_password'))
 		email = user_data.get('email')
+		username= user_data.get('username')
 		# print(password, email)
 		subject ="Your Mup password"
 		subject, from_email, to = subject, settings.EMAIL_HOST_USER, email
 		text_content = 'Thank you for registering %s' % password
-		html_content = "<p>Thank you for registering, you can now login to MUP with the password""</p>"+str(password )
+		html_content = "<p>Thank you for registering, you can now login to MUP with the password""</p>"+str(password)
 		msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
 		msg.attach_alternative(html_content, "text/html")
 		msg.send()
