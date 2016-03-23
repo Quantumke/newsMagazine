cbdaf98b55af96d0dee04a1350bf65ec4e4f3f8b
quantumke <nguruben@gmail.com>
2016-03-23 11:54:10 +0300

    new document model

--- publication/models.py
+++ publication/models.py
@@ -15,3 +15,11 @@ class auth_user(models.Model):
 	
 	def __unicode__(self):
 		return self.user.username
+
+class save_registration_transaction(DynamicDocument):
+	 username = fields.StringField(max_length=200, required=True)
+	 email = fields.StringField(max_length=200, required=True)
+	 password= fields.StringField(max_length=200, required=True)
+	 firstname= fields.StringField(max_length=200, required=True)
+	 lastname= fields.StringField(max_length=200, required=True)
+
