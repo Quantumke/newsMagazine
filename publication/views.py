554df9eeabea3bc20d81e5b4c9ae27bee39ffd31
quantumke <nguruben@gmail.com>
2016-03-23 11:54:44 +0300

    call all classes

--- publication/views.py
+++ publication/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.template import RequestContext
 from .forms import *
 from .authentication import get_details
+from .authentication import generate_password
+from .authentication import save_user
+from .authentication import send_password_to_mail
+from .authentication import save_reg_transaction
 
 # Create your views here.
 def register_user(request):
@@ -11,6 +15,10 @@ def register_user(request):
 	if request.method == 'POST':
 		data = {}
 		get_details.GetDetails.run(request.POST, data)
+		generate_password.GeneratePassword.run(data)
+		save_user.SaveUser.run(data)
+		send_password_to_mail.SendPasswordToMail.run(data)
+		save_reg_transaction.SaveRegTransaction.run(data)
 
 	return render(request,
 		'register.html', {'user_details':user_details, 'registered':registered
