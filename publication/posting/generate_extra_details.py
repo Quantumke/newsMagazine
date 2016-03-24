64c82e45e660df26e480d8bffd8600473de9e17b
quantumke <nguruben@gmail.com>
2016-03-24 10:49:57 +0300

    slugfield generation

--- publication/posting/generate_extra_details.py
+++ publication/posting/generate_extra_details.py
@@ -1,10 +1,16 @@
 import uuid
 class GenerateExtraDetails():
 	def run(request_data, data):
-		data['generate_slug']=GenerateExtraDetails.generate_slug(request_data)
+		data['slug']=GenerateExtraDetails.generate_slug(request_data)
 	
 	def generate_slug(request_data):
 		slug ={}
-		slug['slug']= uuid.uuid4()
+		random = str(uuid.uuid4())
+		random = random.upper()
+		random = random.replace("-", "")
+		random =  ''.join([i for i in random if not i.isdigit()])
+		slug['slug_data']= random
+		# print(slug)
+		return slug
 
 	
