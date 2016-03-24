3741580694482a57fe246614b76a70af9352fb88
quantumke <nguruben@gmail.com>
2016-03-24 10:51:23 +0300

    save post events

---
+++ publication/posting/save_post_transaction.py
@@ -0,0 +1,17 @@
+from publication.models import post_transaction
+class SavePostTransaction():
+	def run(data):
+		post_data= data.get('post_data')
+		postdata = post_transaction(**post_data)
+		slug = data.get('slug')
+		title = post_data.get('title')
+		body = post_data.get('body')
+		category=post_data.get('category')
+		slug_data=slug.get('slug_data')
+		print(title, body, category, slug_data)
+		e = post_transaction(title=title, body=body, category=category, slug=slug_data, posted_by='ben')
+		e.save()
+		data['postdata']=postdata
+
+
+		
