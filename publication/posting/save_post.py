c5804f799982a12508fc10ad58cf9fdebff018c9
quantumke <nguruben@gmail.com>
2016-03-24 10:50:16 +0300

    return bug fixed

--- publication/posting/save_post.py
+++ publication/posting/save_post.py
@@ -1,6 +1,19 @@
+from publication.models import news_posts
 class SavePost():
 	def run (data):
 		post_data= data.get('post_data')
-		generate_slug = data.get('generate_slug')
-		print(generate_slug, post_data)
+		postdata = news_posts(**post_data)
+		slug = data.get('slug')
+		postdata=news_posts(**post_data)
+		title = postdata.title
+		body = postdata.body
+		category=postdata.category
+		slug_data=slug.get('slug_data')
+		print(slug_data)
+		e = news_posts(title=title, body=body, category=category, slug=slug_data, posted_by='ben')
+		e.save()
+		data['postdata']=postdata
+
+
+		
 		
