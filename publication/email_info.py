b7cf7c5a34e012fa15f10477e11f56cc3e739d4a
GodMode <nguruben@gmail.com>
2016-04-02 18:08:10 +0300

    email settings

---
+++ publication/email_info.py
@@ -0,0 +1,6 @@
+#--------------------------------------------------------------------------------------------------------------------python send email()
+EMAIL_USE_TLS = True
+EMAIL_HOST="smtp.sendgrid.net"
+EMAIL_HOST_USER="adlink_mail"
+EMAIL_PORT= 587
+EMAIL_HOST_PASSWORD="vSYBBDXHJBPxKr4"
