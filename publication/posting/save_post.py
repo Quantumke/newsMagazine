4b70fc1d320ff990eea5227453643b9993b182f8
GodMode <nguruben@gmail.com>
2016-04-10 21:31:08 +0300

    image save

--- publication/posting/save_post.py
+++ publication/posting/save_post.py
@@ -2,16 +2,19 @@ from publication.models import news_posts
 class SavePost():
 	def run (data):
 		post_data= data.get('post_data')
+		#image=post_data.get('image')
 		postdata = news_posts(**post_data)
 		slug = data.get('slug')
 		postdata=news_posts(**post_data)
+		files=data.get('files')
+		image=files.get('image')
 		title = postdata.title
 		body = postdata.body
 		category=postdata.category
 		slug_data=slug.get('slug_data')
-		print(slug_data)
-		e = news_posts(title=title, body=body, category=category, slug=slug_data, posted_by='ben')
-		e.save()
+		print(image)
+		#e = news_posts(title=title, body=body, category=category, slug=slug_data, posted_by='ben',image=image)
+		#e.save()
 		data['postdata']=postdata
 
 
